0071f52cad38d9c19385bde7fdb6e9b53c7db3a6
Andreas Kraske <andreaskraske@googlemail.com>
2017-10-20 08:10:24 +0200

    imagebutton scaling grundlage

--- app/src/main/java/com/example/akra/testapp/ActivityGameplayTest.java
+++ app/src/main/java/com/example/akra/testapp/ActivityGameplayTest.java
@@ -9,10 +9,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
-import static com.example.akra.testapp.R.id.buttonTarget;
+import static com.example.akra.testapp.R.id.buttonBackToMainScreen2;
+import static com.example.akra.testapp.R.id.reloadButton;
 
 /**
  * Created by akra on 22.09.2017.
@@ -20,23 +22,31 @@ import static com.example.akra.testapp.R.id.buttonTarget;
 
 public class ActivityGameplayTest extends AppCompatActivity implements View.OnClickListener
 {
+
+    int amuValue = 10;
+
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activitygameplaytest);
 
-        Button buttonTarget = (Button) findViewById(R.id.buttonTarget);
         Button btms2 = (Button) findViewById(R.id.buttonBackToMainScreen2);
         ImageButton iBR = (ImageButton) findViewById(R.id.imageButtonRocket);
+        ImageButton reloadButton = (ImageButton) findViewById(R.id.reloadButton);
+        //ImageButton backgroundButton = (ImageButton) findViewById(R.id.buttonBackground);
+        TextView actualAmmunition = (TextView) findViewById(R.id.textActualAmmu);
 
         iBR.setBackground(null);
         iBR.setOnClickListener(this);
+        reloadButton.setBackground(null);
+        reloadButton.setOnClickListener(this);
+        //backgroundButton.setBackground(null);
+        //backgroundButton.setOnClickListener(this);
         btms2.setOnClickListener(this);
-        buttonTarget.setOnClickListener(this);
+        actualAmmunition.setText(Integer.toString(amuValue));
 
 
-        double randomValueX = Math.random();
-        double randomValueY = Math.random();
+
         iBR.setX(500);
         iBR.setY(900);
 
@@ -44,30 +54,53 @@ public class ActivityGameplayTest extends AppCompatActivity implements View.OnCl
     @Override
     public void onClick(View v)
     {
-        switch(v.getId())
-        {
-            case R.id.buttonBackToMainScreen2:
+        TextView actualAmmunition = (TextView) findViewById(R.id.textActualAmmu);
 
-                finish();
-                break;
+        /*if(v.getId() != R.id.buttonBackToMainScreen2 && v.getId() != R.id.imageButtonRocket && v.getId() != R.id.reloadButton){
+            if(amuValue > 0) {
+                amuValue = amuValue - 1;
+                actualAmmunition.setText(Integer.toString(amuValue));
+            }
+        }
+        else {*/
 
-            case R.id.imageButtonRocket:
-                ButtonPlacement idk = new ButtonPlacement();
-                ImageButton iBR = (ImageButton) findViewById(R.id.imageButtonRocket);
 
-                ButtonPlacement werteRandomen = new ButtonPlacement();
-                iBR.setX(werteRandomen.getRandomZahlX());
-                iBR.setY(werteRandomen.getRandomZahlY());
-                iBR.setScaleX((float)1.5);
-                iBR.setScaleY((float)1.5);
+            switch (v.getId()) {
+                case R.id.buttonBackToMainScreen2:
+                    finish();
+                    break;
 
-            case buttonTarget:
+                case R.id.imageButtonRocket:
+                    if (amuValue > 0) {
+                        ButtonPlacement idk = new ButtonPlacement();
+                        ImageButton iBR = (ImageButton) findViewById(R.id.imageButtonRocket);
 
-                break;
+                        ButtonPlacement werteRandomen = new ButtonPlacement();
+                        iBR.setX(werteRandomen.getRandomZahlX());
+                        iBR.setY(werteRandomen.getRandomZahlY());
+                        iBR.setScaleX((float) 1);
+                        iBR.setScaleY((float) 1);
+                        amuValue = amuValue - 1;
 
+                        actualAmmunition.setText(Integer.toString(amuValue));
+                    } else {
+                        break;
+                    }
+
+                case R.id.reloadButton:
+                    amuValue = 10;
+
+                    actualAmmunition.setText(Integer.toString(amuValue));
+                    break;
+
+                default:
+                    if(amuValue > 0){
+                        amuValue = amuValue - 1;
+                        actualAmmunition.setText(Integer.toString(amuValue));
+
+
+            }
         }
     }
-
-   /* @Override
-    public void String */
 }
+
