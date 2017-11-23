c6541472f570b8ff32155ae4a5ea5deb0e7c335a
Andreas Kraske <andreaskraske@googlemail.com>
2017-11-23 14:52:33 +0100

    small fixes. Buttons still jumping

--- app/src/main/java/com/example/akra/testapp/ActivityGameplayTest.java
+++ app/src/main/java/com/example/akra/testapp/ActivityGameplayTest.java
@@ -21,14 +21,15 @@ import android.widget.Toast;
 public class ActivityGameplayTest extends AppCompatActivity implements View.OnClickListener
 {
 
-    int amuValue = 10;
-    int lifePoints = 9;
+    int amuValue = 20;
+    int lifePoints = 999999;
     String uebergabeString;
     String uebergabeLifepoints;
     String uebergabeScore;
     String uebergabeStufe;
     CountDownTimer target1ScalingInitiative;
     CountDownTimer target2ScalingInitiative;
+    CountDownTimer target3ScalingInitiative;
     CountDownTimer difficulty;
     long score = 0;
     double scoreGain = 1;
@@ -37,8 +38,10 @@ public class ActivityGameplayTest extends AppCompatActivity implements View.OnCl
     float scalingAmount = 0.008F;
     boolean scalingTarget1Started = false;
     boolean scalingTarget2Started = false;
+    boolean scalingTarget3Started = false;
     float iScalerTarget1 = 1F;
     float iScalerTarget2 = 1F;
+    float iScalerTarget3 = 1F;
     int schwierigStufe = 0;
 
 
@@ -51,6 +54,7 @@ public class ActivityGameplayTest extends AppCompatActivity implements View.OnCl
         Button btms2 = (Button) findViewById(R.id.buttonBackToMainScreen2);                         //buttons initialisieren .
         ImageButton iBR1 = (ImageButton) findViewById(R.id.imageButtonRocket);
         ImageButton iBR2 = (ImageButton) findViewById(R.id.imageButtonRocket2);
+        ImageButton iBR3 = (ImageButton) findViewById(R.id.imageButtonRocket3);
         ImageButton reloadButton = (ImageButton) findViewById(R.id.reloadButton);
         ImageButton backgroundButton = (ImageButton)findViewById(R.id.buttonBackground);
         ImageButton blockShots = (ImageButton)findViewById(R.id.buttonBlockShots);
@@ -67,6 +71,7 @@ public class ActivityGameplayTest extends AppCompatActivity implements View.OnCl
 
         iBR1.setBackground(null);
         iBR2.setBackground(null);
+        //iBR3.setBackground(null);
         reloadButton.setBackground(null);
         backgroundButton.setBackground(null);
         blockShots.setBackground(null);
@@ -75,6 +80,7 @@ public class ActivityGameplayTest extends AppCompatActivity implements View.OnCl
         btms2.setOnClickListener(this);
         iBR1.setOnClickListener(this);
         iBR2.setOnClickListener(this);
+        iBR3.setOnClickListener(this);
         backgroundButton.setOnClickListener(this);
         blockShots.setOnClickListener(this);
         reloadButton.setOnClickListener(this);
@@ -90,14 +96,17 @@ public class ActivityGameplayTest extends AppCompatActivity implements View.OnCl
         iBR1.setY(5000);
         iBR2.setX(5000);
         iBR2.setY(5000);
+        iBR3.setX(5000);
+        iBR3.setY(5000);
 
         blockShots.setX(5000F);
         blockShots.setY(5000F);
 
 
-        difficulty = new CountDownTimer(86400000,10000){
+        difficulty = new CountDownTimer(86400000,10000){                    //Schwerigkeitslevel der sich über die Zeit erhöht
             ImageButton iBR1 = (ImageButton)findViewById(R.id.imageButtonRocket);
             ImageButton iBR2 = (ImageButton)findViewById(R.id.imageButtonRocket2);
+            ImageButton iBR3 = (ImageButton)findViewById(R.id.imageButtonRocket3);
 
             @Override
             public void onTick(long millisUntilFinished) {
@@ -106,12 +115,12 @@ public class ActivityGameplayTest extends AppCompatActivity implements View.OnCl
                 uebergabeStufe = String.valueOf(schwierigStufe);
                 currentLevel.setText("Stufe: " + uebergabeStufe);
 
-                if(scalingSpeed > 500) {
-                    scalingSpeed = scalingSpeed - 250;
+                if(scalingSpeed > 1500) {                                                           //erhöhung der Geschwindigkeit
+                    scalingSpeed = scalingSpeed - 200;
                     scalingSteps = scalingSpeed / 200;
                 }
 //################## Target 1 ######################################################################
-                    if(scalingTarget1Started == true){
+                    if(scalingTarget1Started == true) {
                         target1ScalingInitiative.cancel();
                         target1ScalingInitiative = new CountDownTimer(scalingSpeed, scalingSteps) {
 
@@ -136,7 +145,7 @@ public class ActivityGameplayTest extends AppCompatActivity implements View.OnCl
                                 final Handler handler = new Handler();
                                 handler.postDelayed(new Runnable() {
                                     public void run() {
-                                        //ImageButton iBR = (ImageButton) findViewById(R.id.imageButtonRocket);
+                                        ImageButton iBR1 = (ImageButton) findViewById(R.id.imageButtonRocket);
 
                                         ButtonPlacement werteRandomen = new ButtonPlacement();
                                         iBR1.setX(werteRandomen.getRandomZahlX());
@@ -150,6 +159,8 @@ public class ActivityGameplayTest extends AppCompatActivity implements View.OnCl
                                             iBR1.setY(5000);
                                             iBR2.setX(5000);
                                             iBR2.setY(5000);
+                                            iBR3.setX(5000);
+                                            iBR3.setY(5000);
                                             difficulty.cancel();
                                         }
                                     }
@@ -159,9 +170,8 @@ public class ActivityGameplayTest extends AppCompatActivity implements View.OnCl
                         };
                         target1ScalingInitiative.start();
                     }
-                    else{
+                    else{                                                                           //wenn scheibe nicht im Bild ist
                         target1ScalingInitiative = new CountDownTimer(scalingSpeed, scalingSteps) {
-
                             @Override
                             public void onTick(long millisUntilFinished) {                          // alle x milisekunden Scheibe vergrößern.
                                 iScalerTarget1 = iScalerTarget1 + scalingAmount;
@@ -195,6 +205,8 @@ public class ActivityGameplayTest extends AppCompatActivity implements View.OnCl
                                             iBR1.setY(5000);
                                             iBR2.setX(5000);
                                             iBR2.setY(5000);
+                                            iBR3.setX(5000);
+                                            iBR3.setY(5000);
                                             difficulty.cancel();
                                         }
                                     }
@@ -204,7 +216,7 @@ public class ActivityGameplayTest extends AppCompatActivity implements View.OnCl
                         };
                     }
 //################## Target 2 ######################################################################
-                    if(schwierigStufe > 1) {
+                    if(schwierigStufe > 2) {
                         if (scalingTarget2Started == true) {
                             target2ScalingInitiative.cancel();
                             target2ScalingInitiative = new CountDownTimer(scalingSpeed, scalingSteps) {
@@ -244,6 +256,8 @@ public class ActivityGameplayTest extends AppCompatActivity implements View.OnCl
                                                 iBR1.setY(5000);
                                                 iBR2.setX(5000);
                                                 iBR2.setY(5000);
+                                                iBR3.setX(5000);
+                                                iBR3.setY(5000);
                                                 difficulty.cancel();
                                             }
                                         }
@@ -254,9 +268,10 @@ public class ActivityGameplayTest extends AppCompatActivity implements View.OnCl
                             target2ScalingInitiative.start();
                         } else {
                             ButtonPlacement werteRandomen = new ButtonPlacement();
-                            iBR2.setX(werteRandomen.getRandomZahlX());
-                            iBR2.setY(werteRandomen.getRandomZahlY());
-
+                            if(schwierigStufe == 3) {
+                                iBR2.setX(werteRandomen.getRandomZahlX());
+                                iBR2.setY(werteRandomen.getRandomZahlY());
+                            }
                             target2ScalingInitiative = new CountDownTimer(scalingSpeed, scalingSteps) {
                                 @Override
                                 public void onTick(long millisUntilFinished) {                          // alle x milisekunden Scheibe vergrößern.
@@ -293,6 +308,8 @@ public class ActivityGameplayTest extends AppCompatActivity implements View.OnCl
                                                 iBR1.setY(5000);
                                                 iBR2.setX(5000);
                                                 iBR2.setY(5000);
+                                                iBR3.setX(5000);
+                                                iBR3.setY(5000);
                                                 difficulty.cancel();
                                             }
                                         }
@@ -302,6 +319,112 @@ public class ActivityGameplayTest extends AppCompatActivity implements View.OnCl
                             };target2ScalingInitiative.start();
                         }
                     }
+//################## Target 3 ######################################################################
+                if(schwierigStufe > 4) {
+                    if (scalingTarget3Started == true) {
+                        target3ScalingInitiative.cancel();
+                        target3ScalingInitiative = new CountDownTimer(scalingSpeed, scalingSteps) {
+
+                            @Override
+                            public void onTick(long millisUntilFinished) {
+                                iScalerTarget3 = iScalerTarget3 + scalingAmount;
+                                iBR3.setScaleX(iScalerTarget3);
+                                iBR3.setScaleY(iScalerTarget3);
+                                scalingTarget3Started = true;
+                            }
+
+                            @Override
+                            public void onFinish() {
+                                lifePoints = lifePoints - 1;
+                                uebergabeLifepoints = String.valueOf(lifePoints);
+                                currentLifepoints.setText(uebergabeLifepoints);
+                                iScalerTarget3 = 1;
+
+                                iBR3.setX(5000F);
+                                iBR3.setY(5000F);
+
+                                final Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    public void run() {
+                                        ImageButton iBR3 = (ImageButton) findViewById(R.id.imageButtonRocket3);
+
+                                        ButtonPlacement werteRandomen = new ButtonPlacement();
+                                        iBR3.setX(werteRandomen.getRandomZahlX());
+                                        iBR3.setY(werteRandomen.getRandomZahlY());
+                                        iBR3.setScaleX((float) 1);
+                                        iBR3.setScaleY((float) 1);
+                                        if (lifePoints > 0) {
+                                            target3ScalingInitiative.start();
+                                        } else {
+                                            iBR1.setX(5000);
+                                            iBR1.setY(5000);
+                                            iBR2.setX(5000);
+                                            iBR2.setY(5000);
+                                            iBR3.setX(5000);
+                                            iBR3.setY(5000);
+                                            difficulty.cancel();
+                                        }
+                                    }
+                                }, (long) (1000 + (Math.random() * 2000)));
+
+                            }
+                        };
+                        target3ScalingInitiative.start();
+                    } else {
+                        ButtonPlacement werteRandomen = new ButtonPlacement();
+                        if(schwierigStufe == 5) {
+                            iBR3.setX(werteRandomen.getRandomZahlX());
+                            iBR3.setY(werteRandomen.getRandomZahlY());
+                        }
+
+                        target3ScalingInitiative = new CountDownTimer(scalingSpeed, scalingSteps) {
+                            @Override
+                            public void onTick(long millisUntilFinished) {                          // alle x milisekunden Scheibe vergrößern.
+                                iScalerTarget3 = iScalerTarget3 + scalingAmount;
+                                iBR3.setScaleX(iScalerTarget3);
+                                iBR3.setScaleY(iScalerTarget3);
+                                scalingTarget3Started = true;
+                            }
+
+                            @Override
+                            public void onFinish() {                                                //Wenn Scheibe maximal skaliert hat.
+                                lifePoints = lifePoints - 1;                                        //Lebenspunkt abziehen
+                                uebergabeLifepoints = String.valueOf(lifePoints);
+                                currentLifepoints.setText(uebergabeLifepoints);
+                                iScalerTarget3 = 1;
+
+                                iBR3.setX(5000F);                                                    //Scheibe aus Spielfeld bewegen
+                                iBR3.setY(5000F);
+
+                                final Handler handler = new Handler();                              //nach 1 - 1500 ms wird selber Countdowntimer erneut gestartet
+                                handler.postDelayed(new Runnable() {
+                                    public void run() {
+                                        ImageButton iBR3 = (ImageButton) findViewById(R.id.imageButtonRocket2);
+
+                                        ButtonPlacement werteRandomen = new ButtonPlacement();
+                                        iBR3.setX(werteRandomen.getRandomZahlX());
+                                        iBR3.setY(werteRandomen.getRandomZahlY());
+                                        iBR3.setScaleX((float) 1);
+                                        iBR3.setScaleY((float) 1);
+                                        if (lifePoints > 0) {
+                                            target3ScalingInitiative.start();                         //
+                                        } else {
+                                            iBR1.setX(5000);
+                                            iBR1.setY(5000);
+                                            iBR2.setX(5000);
+                                            iBR2.setY(5000);
+                                            iBR3.setX(5000);
+                                            iBR3.setY(5000);
+                                            difficulty.cancel();
+                                        }
+                                    }
+                                }, (long) (1000 + (Math.random() * 2000)));
+
+                            }
+                        };target3ScalingInitiative.start();
+                    }
+                }
+
 //################## letzter Button zu Ende ########################################################
 
             }
@@ -372,19 +495,40 @@ public class ActivityGameplayTest extends AppCompatActivity implements View.OnCl
                         break;
                     }
 
+                case R.id.imageButtonRocket3:
+                    if(amuValue >0)
+                    {
+                        amuValue = amuValue - 1;
+                        uebergabeString = String.valueOf(amuValue);
+                        actualAmmunition.setText(uebergabeString);
+
+                        lifePoints++;
+                        target3ScalingInitiative.cancel();
+                        target3ScalingInitiative.onFinish();
+                        scalingTarget3Started = false;
+
+                        score = Math.round((long) (score + scoreGain));
+                        uebergabeScore = String.valueOf(score);
+                        currentScore.setText("Punktzahl: " + uebergabeScore);
+                        break;
+                    }
+                    else{
+                        break;
+                    }
+
 
                 case R.id.reloadButton:                                                             //Nachladen
                     ImageButton blockShots = (ImageButton)findViewById(R.id.buttonBlockShots);
                     blockShots.setX(0);
                     blockShots.setY(80);
 
-                    CountDownTimer reloadCountDown = new CountDownTimer(2500,5){
+                    CountDownTimer reloadCountDown = new CountDownTimer(1500,5){
                         int i = 0;
                         @Override
                         public void onTick(long milliSekunden){                                     //Progress Bar beim Nachladen füllen
                             ProgressBar reloadBar = (ProgressBar)findViewById(R.id.progressBarReload);
                             i++;
-                            reloadBar.setProgress((int)i*100/(2500/15));
+                            reloadBar.setProgress((int)i*100/(1500/15));
 
                         }
 
@@ -393,7 +537,7 @@ public class ActivityGameplayTest extends AppCompatActivity implements View.OnCl
                             TextView actualAmmunition = (TextView) findViewById(R.id.textActualAmmu);
                             ProgressBar reloadBar = (ProgressBar)findViewById(R.id.progressBarReload);
 
-                            amuValue = 10;
+                            amuValue = 20;
                             uebergabeString = String.valueOf(amuValue);
                             actualAmmunition.setText(uebergabeString);
                             reloadBar.setProgress(0);
