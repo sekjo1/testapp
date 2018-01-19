1b61681ae1ec8fbaccb2d3c2c31f2fe13a3b70c2
Andreas Kraske <andreaskraske@googlemail.com>
2018-01-19 08:57:07 +0100

    neue Grafiken eingefügt

--- app/src/main/java/com/example/akra/testapp/Constants.java
+++ app/src/main/java/com/example/akra/testapp/Constants.java
@@ -6,8 +6,7 @@ package com.example.akra.testapp;
 
 public class Constants
 {
-    //private static final String ROOT_URL = "http://rund-ums-schiessen.de/bsprojekt/v1/";
-    private static final String ROOT_URL= "localhost";
+    private static final String ROOT_URL = "http://rund-ums-schiessen.de/bsprojekt/v1/";
     public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
     public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
 }
